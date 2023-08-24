a858ad6ca041058260b78a7f92b0a4f92787fdea
Loren Yu <loren@navapbc.com>
2023-08-23 17:33:18 -0700

    Pull latest changes from infra template

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -1,15 +1,18 @@
 locals {
   # Machine readable project name (lower case letters, dashes, and underscores)
-  project_name = "platform-nextjs"
+  # This will be used in names of AWS resources
+  project_name = "<PROJECT_NAME>"
 
-  # Project owner
-  owner = "platform-admins"
+  # Project owner (e.g. navapbc). Used for tagging infra resources.
+  owner = "<OWNER>"
 
   # URL of project source code repository
-  code_repository_url = "git@github.com:navapbc/platform-test-nextjs.git"
+  code_repository_url = "<REPO_URL>"
 
-  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1)
-  default_region = "us-east-1"
+  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1).
+  # This is dependent on where your project is located (if regional)
+  # otherwise us-east-1 is a good default
+  default_region = "<DEFAULT_REGION>"
 
   github_actions_role_name = "${local.project_name}-github-actions"
 }
