2ed792bb7ac688e3fbb5d8a0521fcfdce903445b
Ryan Lewis <ryanlewis@navapbc.com>
2024-07-08 21:10:43 -0700

    add explaining comments on pa11y config files

--- accessibility/.pa11yci-mobile.js
+++ accessibility/.pa11yci-mobile.js
@@ -1,23 +1,42 @@
 module.exports = {
-  "defaults": {
-    "timeout": 240000,
-    "runners": ["axe"],
-    "ignore": ["color-contrast"],
-    "concurrency": 1,
-    "chromeLaunchConfig": {
-      "ignoreHTTPSErrors": true,
-      "args": ["--disable-dev-shm-usage", "--no-sandbox"]
-    },
-    "viewport": {
-      "width": 390,
-      "height": 844,
-      "mobile": true
-    },
-    "actions": [
-      "wait for element #main-content to be visible",
-      "screen capture screenshots-output/mobile-main-view.png",
-      "click element .usa-navbar button",
-      "screen capture screenshots-output/mobile-expand-menu.png"
-    ]
-  }
-};
+    "defaults": {
+      // The maximum time allowed for the entire test run, in milliseconds
+      "timeout": 240000,
+
+      // The accessibility testing runners to use; "axe" is a common choice
+      "runners": ["axe"],
+
+      // An array of rules to ignore during the accessibility test
+      // pa11y reports false positive color-contrast issues https://github.com/pa11y/pa11y/issues/633
+      "ignore": ["color-contrast"],
+
+      // The number of tests to run concurrently
+      "concurrency": 1,
+
+      // Configuration for launching Chrome
+      "chromeLaunchConfig": {
+        // Ignore HTTPS errors (useful for testing environments with self-signed certificates)
+        "ignoreHTTPSErrors": true,
+        "args": [
+          // Prevent chrome from crashing when using shared memory for large files/data
+          "--disable-dev-shm-usage",
+          // Disables Chrome's sandboxing for compatibility with environments where sandboxing might cause issues.
+          "--no-sandbox"
+        ]
+      },
+
+      // Configuration for the viewport size and type
+      "viewport": {
+        "width": 390,
+        "height": 844,
+        "mobile": true ,
+
+      // Actions to perform during the test
+      "actions": [
+        "wait for element #main-content to be visible",
+        "screen capture screenshots-output/mobile-main-view.png",
+        "click element .usa-navbar button",
+        "screen capture screenshots-output/mobile-expand-menu.png"
+      ]
+    }
+  };
