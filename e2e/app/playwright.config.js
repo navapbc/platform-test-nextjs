74d5e168a20bde7eef9b6c6997352528d4c6a599
Ryan Lewis <ryanlewis@navapbc.com>
2024-07-11 15:15:27 -0700

    remove testDir in derived

--- e2e/app/playwright.config.js
+++ e2e/app/playwright.config.js
@@ -1,19 +1,16 @@
+import { defineConfig, devices } from '@playwright/test';
+
 import baseConfig from '../playwright.config';
-import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   ...baseConfig,
 
-  // app specific overrides can go here
-  testDir: "./tests",
-  use: {
-    baseURL: process.env.BASE_URL,
-  }
-//   projects: [
-//     ...baseConfig.projects,
-//     {
-//         name: "webkit",
-//         use: { ...devices["Desktop Safari"] },
-//       },
-//   ]
+  projects: [
+    ...baseConfig.projects,
+    // add Safari for derived app config
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
 });
