c47eebf80330779a53a8ae7a5b479f503a86ceb9
Ryan Lewis <ryanlewis@navapbc.com>
2024-07-11 16:02:27 -0700

    add a11y test

--- e2e/app/tests/index.spec.js
+++ e2e/app/tests/index.spec.js
@@ -1,5 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
+import AxeBuilder from '@axe-core/playwright';
+
 test.describe('Generic Webpage Tests', () => {
   test('should load the webpage and verify title', async ({ page }) => {
     const response = await page.goto('/');
@@ -13,12 +15,18 @@ test.describe('Generic Webpage Tests', () => {
     await page.screenshot({ path: 'example-screenshot.png', fullPage: true });
   });
 
-    // Example test of finding a an html element on the index/home page
-//   test('should check for an element to be visible', async ({ page }) => {
-//     await page.goto('/');
-//     const element = page.locator('h1');
-//     await expect(element).toBeVisible();
-//   });
-
+  // Add an accessibility test
+  test('should not have any automatically detectable accessibility issues', async ({ page }) => {
+    await page.goto('/');
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+  
+  // Example test of finding a an html element on the index/home page
+  // test('should check for an element to be visible', async ({ page }) => {
+  //   await page.goto('/');
+  //   const element = page.locator('h1');
+  //   await expect(element).toBeVisible();
+  // });
 
 });
