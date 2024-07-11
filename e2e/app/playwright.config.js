06ad3ccd5f47ebcb41b581a4f421e77cb1f6e100
Ryan Lewis <ryanlewis@navapbc.com>
2024-07-11 11:23:25 -0700

    update to run package.json script with env var

--- e2e/app/playwright.config.js
+++ e2e/app/playwright.config.js
@@ -3,5 +3,17 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   ...baseConfig,
+
   // app specific overrides can go here
+  testDir: "./tests",
+  use: {
+    baseURL: process.env.BASE_URL,
+  }
+//   projects: [
+//     ...baseConfig.projects,
+//     {
+//         name: "webkit",
+//         use: { ...devices["Desktop Safari"] },
+//       },
+//   ]
 });
