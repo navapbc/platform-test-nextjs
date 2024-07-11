9e080ee6a3e55241949f3d11b7d29763bce7f94d
Ryan Lewis <ryanlewis@navapbc.com>
2024-07-11 16:02:51 -0700

    add comment to playwright a11y testing

--- e2e/app/tests/index.spec.js
+++ e2e/app/tests/index.spec.js
@@ -15,13 +15,13 @@ test.describe('Generic Webpage Tests', () => {
     await page.screenshot({ path: 'example-screenshot.png', fullPage: true });
   });
 
-  // Add an accessibility test
+  // https://playwright.dev/docs/accessibility-testing
   test('should not have any automatically detectable accessibility issues', async ({ page }) => {
     await page.goto('/');
     const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
     expect(accessibilityScanResults.violations).toEqual([]);
   });
-  
+
   // Example test of finding a an html element on the index/home page
   // test('should check for an element to be visible', async ({ page }) => {
   //   await page.goto('/');
