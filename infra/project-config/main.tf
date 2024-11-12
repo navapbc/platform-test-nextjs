33a645b793e9fc1b360ef98c4f56c92ac9e1d593
Tanner Doshier <git@doshitan.com>
2024-11-12 17:44:33 -0500

    Manually update base to version v0.12.4-45-gde5a358

--- infra/project-config/main.tf
+++ infra/project-config/main.tf
@@ -1,14 +1,17 @@
 locals {
   # Machine readable project name (lower case letters, dashes, and underscores)
+  # This will be used in names of AWS resources
   project_name = "platform-nextjs"
 
-  # Project owner
+  # Project owner (e.g. navapbc). Used for tagging infra resources.
   owner = "platform-admins"
 
   # URL of project source code repository
   code_repository_url = "git@github.com:navapbc/platform-test-nextjs.git"
 
-  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1)
+  # Default AWS region for project (e.g. us-east-1, us-east-2, us-west-1).
+  # This is dependent on where your project is located (if regional)
+  # otherwise us-east-1 is a good default
   default_region = "us-east-1"
 
   github_actions_role_name = "${local.project_name}-github-actions"
